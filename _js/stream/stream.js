958534858ef5c93aa8b52594de977f38f6caa32f
Rémi Parmentier <remi@hteumeuleu.fr>
2024-03-06 14:01:39 +0100

    feat: init stream page (wip)

---
+++ _js/stream/stream.js
@@ -0,0 +1,84 @@
+---
+layout: null
+permalink: "/assets/js/stream.js"
+---
+{% include_relative _preview.js %}
+{% include_relative _pd-usb.min.js %}
+
+class streamApp {
+
+	constructor() {
+		this.width = 400;
+		this.height = 240;
+		this.fps = 30;
+		this.preview = new pdvPreview();
+	}
+
+}
+
+let device;
+
+async function connectToPlaydate() {
+	device = await pdusb.requestConnectPlaydate();
+	if (device == null)
+    	throw new Error('Could not find playdate')
+
+    await device.open();
+    const serial = await device.getSerial();
+}
+
+const w = pdusb.PLAYDATE_WIDTH;
+const h = pdusb.PLAYDATE_HEIGHT;
+const global_bitmap = new Uint8Array(w * h).fill(1);
+
+async function sendBitmap() {
+	try {
+		const frame = app.preview.getFrame();
+		const dataRGB = frame.data;
+		for (let i = 0; i < dataRGB.length; i += 4) {
+			let value = dataRGB[i];
+			if(value != 0) {
+				value = 1;
+			}
+			global_bitmap[i/4] = value;
+		}
+		await device.sendBitmapIndexed(global_bitmap);
+		setTimeout(sendBitmap, (1000 / 24));
+	}
+	catch(e) {
+		console.warn(e.message);
+	}
+}
+
+async function getCamera() {
+	navigator.mediaDevices.getUserMedia({
+		audio: true,
+		video: {
+			width: { min: 400 },
+			height: { min: 240 },
+			facingMode: "user",
+		},
+	}).then((mediaStream) => {
+		const video = document.querySelector("video");
+		video.srcObject = mediaStream;
+		video.onloadedmetadata = () => {
+			video.play();
+		};
+	}).catch((err) => {
+	// always check for errors at the end.
+		console.error(`${err.name}: ${err.message}`);
+	});
+}
+
+async function getHelp() {
+
+}
+
+document.addEventListener('DOMContentLoaded', e => {
+	if(!navigator.serial) {
+		document.querySelector('.warning').style.display = 'block';
+	} else {
+		document.querySelector('.warning').remove();
+	}
+	window.app = new streamApp();
+})
